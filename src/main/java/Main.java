afe9064d1f678d5f3336baacb2900dd78e78f2c7
eejonghwa <teriyaki970326@gmail.com>
2023-09-18 23:40:09 +0900

    텍게 요구사항 3번

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,23 @@ public class Main {
         while(true){
             System.out.print("명령어 : ");
             String command = scan.nextLine();
+            String title = "";
+            String content = "";
             if(command.equals("exit")){
                 System.out.println("프로그램을 종료합니다.");
                 break;
             }else if(command.equals("add")){
                 System.out.print("게시물 제목을 입력해주세요 : ");
-                String title = scan.nextLine();
+                title = scan.nextLine();
                 System.out.print("게시물 내용을 입력해주세요 : ");
-                String content = scan.nextLine();
+                content = scan.nextLine();
                 System.out.println("게시물이 등록되었습니다.");
+            }else if(command.equals("list")){
+                System.out.println("==================");
+                System.out.printf("제목 : %s",title);
+                System.out.printf("내용 : %s",content);
+                System.out.println("==================");
+
             }
         }
     }
