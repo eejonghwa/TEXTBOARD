687bd78a7679df2ad7ca13577c51f870ba85b994
eejonghwa <teriyaki970326@gmail.com>
2023-09-18 23:48:36 +0900

    텍게 요구사항3 다건 조회

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,31 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        while(true){
+        ArrayList<String> titles = new ArrayList<>();
+        ArrayList<String> contents = new ArrayList<>();
+        while (true) {
             System.out.print("명령어 : ");
             String command = scan.nextLine();
-            String title = "";
-            String content = "";
-            if(command.equals("exit")){
+            if (command.equals("exit")) {
                 System.out.println("프로그램을 종료합니다.");
                 break;
-            }else if(command.equals("add")){
+            } else if (command.equals("add")) {
                 System.out.print("게시물 제목을 입력해주세요 : ");
-                title = scan.nextLine();
+                titles.add(scan.nextLine());
                 System.out.print("게시물 내용을 입력해주세요 : ");
-                content = scan.nextLine();
-                System.out.println("게시물이 등록되었습니다.");
-            }else if(command.equals("list")){
-                System.out.println("==================");
-                System.out.printf("제목 : %s",title);
-                System.out.printf("내용 : %s",content);
-                System.out.println("==================");
+                contents.add(scan.nextLine());
 
+                System.out.println("게시물이 등록되었습니다.");
+            } else if (command.equals("list")) {
+                System.out.println("==================");
+                for (int i = 0; i < titles.size(); i++){
+                    System.out.printf("제목 : %s", titles.get(i));
+                    System.out.printf("내용 : %s", contents.get(i));
+                    System.out.println("==================");
+                }
             }
         }
     }
