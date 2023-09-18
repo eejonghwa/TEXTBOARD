cada57c1d68e71b126102df0bf1732f9e110c227
eejonghwa <teriyaki970326@gmail.com>
2023-09-18 23:35:59 +0900

    텍게 요구사항 2번

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,12 @@ public class Main {
             if(command.equals("exit")){
                 System.out.println("프로그램을 종료합니다.");
                 break;
+            }else if(command.equals("add")){
+                System.out.print("게시물 제목을 입력해주세요 : ");
+                String title = scan.nextLine();
+                System.out.print("게시물 내용을 입력해주세요 : ");
+                String content = scan.nextLine();
+                System.out.println("게시물이 등록되었습니다.");
             }
         }
     }
