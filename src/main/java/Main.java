7b7d686777adf72d19d9896bbcdac16c98791087
eejonghwa <teriyaki970326@gmail.com>
2023-09-20 23:41:26 +0900

    텍게 요구사항 6번 수정 삭제 함수사용

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,7 +22,7 @@ public class Main {
                 System.out.print("게시물 내용을 입력해주세요 : ");
                 String content = scan.nextLine();
 
-                Article article = new Article(title, content);
+                Article article = new Article(lastArticleId, title, content);
                 articles.add(article);
 
                 System.out.println("게시물이 등록되었습니다.");
@@ -33,8 +33,8 @@ public class Main {
 
                     Article article = articles.get(i);
 
-                    System.out.printf("번호 : %d", i + 1);
-                    System.out.printf("제목 : %s", article.getTitle());
+                    System.out.printf("번호 : %d\n", article.getId());
+                    System.out.printf("제목 : %s\n", article.getTitle());
                     System.out.println("==================");
                 }
             } else if (command.equals("update")) {
@@ -43,59 +43,41 @@ public class Main {
 
                 scan.nextLine();
 
-                boolean isExist = false;
+                Article article = findById(targetId);
 
-                for (int i = 0; i < articles.size(); i++) {
-                    Article article = articles.get(i);
-                    if (targetId == article.getId()) {
-                        System.out.print("제목 : ");
-                        String newTitle = scan.nextLine();
-                        System.out.print("내용 : ");
-                        String newContent = scan.nextLine();
+                if (article == null) {
+                    System.out.println("존재하지않는 게시물");
+                } else {
+                    System.out.print("수정할 게시물 제목 : ");
+                    String newTitle = scan.nextLine();
+                    System.out.print("수정할 게시물 내용 : ");
+                    String newContent = scan.nextLine();
 
-                        Article newArticle = new Article(targetId, newTitle, newContent);
-                        articles.set(i, newArticle);
+                    article.setTitle(newTitle);
+                    article.setContent(newContent);
 
-                        System.out.println("수정이 완료되었습니다.");
-                        isExist = true;
-                    }
-
+                    System.out.println("수정이 완료되었습니다.");
                 }
-
-                if (isExist == false) {
-                    System.out.println("없는 게시물입니다.");
-                }
-
 
             } else if (command.equals("delete")) {
                 System.out.print("삭제할 게시물 번호 : ");
                 int targetId = scan.nextInt();
 
                 scan.nextLine();
-
-                boolean isExist = false;
-
-                for (int i = 0; i < articles.size(); i++) {
-                    Article article = articles.get(i);
-
-                    if (targetId == article.getId()) {
-                        articles.remove(i);
-                        System.out.printf("%d번 게시물이 삭제되었습니다.\n", targetId);
-                        isExist = true;
-                    }
-                }
-
-                if (isExist == false) {
-                    System.out.println("없는 게시물입니다.");
+                Article article = findById(targetId);
+                if (article == null) {
+                    System.out.println("존재하지않는 게시물");
+                } else {
+                    articles.remove(article);
                 }
             } else if (command.equals("detail")) {
                 int targetId = scan.nextInt();
                 scan.nextLine();
                 Article article = findById(targetId);
 
-                if(article == null){
+                if (article == null) {
                     System.out.println("존재하지 않는 게시물");
-                }else{
+                } else {
                     System.out.println("==================");
                     System.out.printf("번호 : %d\n", article.getId());
                     System.out.printf("제목 : %s\n", article.getTitle());
